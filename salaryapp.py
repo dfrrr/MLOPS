5123cfdb264979f0cfb8ae9b4cb6c8703a98dab5
dfrrr <63469480+dfrrr@users.noreply.github.com>
2021-09-20 17:27:25 +0530

    Update salaryapp.py

--- salaryapp.py
+++ salaryapp.py
@@ -16,7 +16,7 @@ print("""
      -------------------------------------------------------------------------------------------------------------------------------------------------
      
 """)
-prediction = mind.predict([[3]])
+prediction = mind.predict([[0]])
 print("Ypour Expected Salary :" ,prediction,)
 #while(True):
  #   exp = input("Enter Your Relative Experience In ML: ")
